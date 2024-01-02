f8e5413878d351a8682f68c58fb87bf811a5cd06
alijr2018 <alijr_2018@proton.me>
2024-01-02 14:51:42 +0100

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,10 @@ import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+  .then(([photoResponse, userResponse]) => {
+    const photo = photoResponse.body;
+    const { firstName, lastName } = userResponse;
+
     })
     .catch((error) => {
       console.error('Signup system offline');
