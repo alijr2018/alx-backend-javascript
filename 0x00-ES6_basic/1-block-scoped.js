fb6bfd3555559b6230337458eca65d083c0bd0e4
alijr2018 <alijr_2018@proton.me>
2024-01-03 17:04:55 +0100

    lol

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
 	var task2 = true;
 
 	if (trueOrFalse) {
-		/* eslint-disable no-unused-vars */
+		// eslint-disable-next-line no-unused-vars, no-shadow
 		const task = true;
+		// eslint-disable-next-line no-unused-vars, no-shadow
 		const task2 = false;
 	}
 
