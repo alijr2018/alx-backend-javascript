6d68e4bc9f49c60c5af80339dc620abeb0df2a0b
alijr2018 <alijr_2018@proton.me>
2024-01-03 18:11:59 +0100

    lol

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,13 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      const photo = photoResponse.body; // Extracting 'body' from photo response
-      const { firstName, lastName } = userResponse; // Destructuring user response
-
-      console.log(`${photo} ${firstName} ${lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body.firstName} ${user.body.lastName}`);
     })
     .catch(() => {
       console.error('Signup system offline');
     });
 }
+
+export default handleProfileSignup;
