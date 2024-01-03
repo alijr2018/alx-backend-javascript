064e6dd83e142416a3f4ea003c732032ae9bb5ee
alijr2018 <alijr_2018@proton.me>
2024-01-03 18:07:12 +0100

    lol

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,16 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   promise
-    .then((response) => {
+    .then(() => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch((error) => {
-      console.error(error);
+    .catch(() => {
+      console.error('Got a response from the API');
       return new Error();
     });
 }
+
+export default handleResponseFromAPI;
