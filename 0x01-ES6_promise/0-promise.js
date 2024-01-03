0c7e6733229196841b3b264a1312d31990868331
alijr2018 <alijr_2018@proton.me>
2024-01-03 18:00:26 +0100

    what's

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve('API response data');
-    });
-  }
-  
+  return new Promise((resolve) => {
+    resolve('API response data');
+  });
+}
