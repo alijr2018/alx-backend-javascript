ce21ecfa4a5040f0845669ad5bf4d6f8617a1a98
alijr2018 <alijr_2018@proton.me>
2024-01-03 17:53:04 +0100

    done

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,12 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
 
+  // Populate allEmployees object
   for (const department in employeesList) {
     allEmployees[department] = [...employeesList[department]];
   }
 
+  // Return object with allEmployees and getNumberOfDepartments method
   return {
     allEmployees,
     getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
