35c2fb4e1bc1c337dcd98bc8b3617a5f275ce6d3
alijr2018 <alijr_2018@proton.me>
2024-01-04 20:51:20 +0100

    as

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,26 @@
-export default class Building {
+class Building {
   constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Abstract class "Building" cannot be instantiated directly');
+    }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-enable class-methods-use-this */
 }
+
+export default Building;
