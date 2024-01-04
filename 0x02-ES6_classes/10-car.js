66d97b9d2cc5f599987eab8ee4300e7fce39d6fe
alijr2018 <alijr_2018@proton.me>
2024-01-04 21:08:32 +0100

    as

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,40 +1,26 @@
-const carData = Symbol('carData');
-
-export default class Car {
+class Car {
   constructor(brand, motor, color) {
-    this[carData] = {
-      _brand: brand,
-      _motor: motor,
-      _color: color,
-    };
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   get brand() {
-    return this[carData]._brand;
-  }
-
-  set brand(newBrand) {
-    this[carData]._brand = newBrand;
+    return this._brand;
   }
 
   get motor() {
-    return this[carData]._motor;
-  }
-
-  set motor(newMotor) {
-    this[carData]._motor = newMotor;
+    return this._motor;
   }
 
   get color() {
-    return this[carData]._color;
-  }
-
-  set color(newColor) {
-    this[carData]._color = newColor;
+    return this._color;
   }
 
   cloneCar() {
-    const { _brand, _motor, _color } = this[carData];
-    return new this.constructor(_brand, _motor, _color);
+    const newCar = new Car(this._brand, this._motor, this._color);
+    return newCar;
   }
 }
+
+export default Car;
