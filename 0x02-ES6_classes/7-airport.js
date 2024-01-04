2739627f3c5c0caca40f4d67c505d2ae4618ca4d
alijr2018 <alijr_2018@proton.me>
2024-01-04 19:50:57 +0100

    lol

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,27 @@
 export default class Airport {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      this._name = newName;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    set code(newCode) {
+      this._code = newCode;
+    }
+  
+    get [Symbol.toStringTag]() {
+        return this._code;
+    }
   }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    this._name = newName;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newCode) {
-    this._code = newCode;
-  }
-
-  toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
-  }
-}
+  
