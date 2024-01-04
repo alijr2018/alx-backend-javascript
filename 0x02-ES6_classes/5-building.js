536248eeba169f480a57594b1f2e394eeb6049c1
alijr2018 <alijr_2018@proton.me>
2024-01-04 20:54:57 +0100

    as

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,30 @@
 class Building {
-  constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Abstract class "Building" cannot be instantiated directly');
+    constructor(sqft) {
+      if (this.constructor === Building) {
+        throw new Error('Abstract class "Building" cannot be instantiated directly');
+      }
+  
+      if (!this.evacuationWarningMessage || typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+  
+      this.sqft = sqft;
     }
-
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  
+    /**
+     * @param {Number} sqft
+     */
+    set sqft(sqft) {
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = sqft;
     }
-
-    this._sqft = sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
+  
+    get sqft() {
+      return this._sqft;
     }
-    this._sqft = sqft;
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
-
-export default Building;
+  
+  export default Building;
+  
