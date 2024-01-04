617dbe79e1d001d22227b65db0922988d6403ebd
alijr2018 <alijr_2018@proton.me>
2024-01-04 21:03:17 +0100

    as

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,7 +13,6 @@ export class HolbertonClass {
   }
 }
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -30,7 +29,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - \ ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} -  ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
@@ -44,4 +43,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents ;
+export default listOfStudents;
