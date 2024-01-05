a77b11bd116a3ee767b0a8b9a9ceaab1fe335206
alijr2018 <alijr_2018@proton.me>
2024-01-05 10:38:40 +0100

    lo

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  cloneCar() {
+    return super.cloneCar();
+  }
+}
