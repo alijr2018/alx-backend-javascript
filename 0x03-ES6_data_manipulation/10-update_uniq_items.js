6e2d707d30c66a19bf0cce027cc12a0f2a6f54f3
alijr2018 <alijr_2018@proton.me>
2024-01-09 20:11:45 +0100

    fix

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if ((map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (var [key, value] of map.entries()) {
+  for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
     }
