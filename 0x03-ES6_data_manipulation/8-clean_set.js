2952ee6e3d2c77c331ccb91cfc59867104ed1c72
alijr2018 <alijr_2018@proton.me>
2024-01-09 19:58:37 +0100

    try

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,16 @@
 export default function cleanSet(set, startString) {
-  const result = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+    const parts = [];
+    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+      return '';
     }
+    for (const value of set.values()) {
+      if (typeof value === 'string' && value.startsWith(startString)) {
+        const valueSubStr = value.substring(startString.length);
+  
+        if (valueSubStr && valueSubStr !== value) {
+          parts.push(valueSubStr);
+        }
+      }
+    }
+    return parts.join('-');
   }
-  return result.join('-');
-}
