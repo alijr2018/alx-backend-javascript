1bf8d48787b6fb114d1288d11a3136d516d836af
alijr2018 <alijr_2018@proton.me>
2024-01-10 15:15:29 +0100

    task_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+export interface Student {
+    firstName:string;
+    lastName:string;
+    age:number;
+    location:string;
+}
+    const student1: Student = {
+        firstName: "Ali",
+        lastName: "Jr",
+        age: 23,
+        location:"Casblanca, Morocco",
+    };
+    const student2: Student = {
+        firstName: "Abdel",
+        lastName: "Rj",
+        age: 21,
+        location:"Rabat, Morocco",
+    };
+
+    
