62f8376c1924b7a01fbaff6f9293544fd1ffb010
alijr2018 <alijr_2018@proton.me>
2024-02-26 16:45:20 +0100

    checking

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,19 +2,19 @@
 
 process.stdin.setEncoding('utf8');
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const input = data.trim();
 
   if (input.toLowerCase() === 'exit') {
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
     process.exit(0);
   }
 
   if (input !== '') {
     console.log(`Your name is: ${input}`);
   } else {
-    console.log("Your name is: ");
+    console.log('Your name is: ');
   }
 });
