b558ba1a70315467715d53ee30a380938c86cced
alijr2018 <alijr_2018@proton.me>
2024-02-29 21:07:02 +0100

    checking[Hc

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+// 1-calcul.test.js
+
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function () {
+    it('should add two numbers when type is SUM', function () {
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should subtract b from a when type is SUBTRACT', function () {
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should divide a by b when type is DIVIDE', function () {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" when dividing by 0', function () {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+
+    it('should throw an error for an invalid type', function () {
+        assert.throws(() => calculateNumber('INVALID_TYPE', 1.4, 4.5), Error);
+    });
+});
