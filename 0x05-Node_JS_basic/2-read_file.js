6042f58bcb2154a8996ddc29a2f26644d649a9ac
alijr2018 <alijr_2018@proton.me>
2024-03-01 19:06:18 +0100

    checking

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,35 +4,38 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = fs.readFileSync(path, 'utf8').split('\n').filter(Boolean);
 
-    const rows = data.trim().split('\n');
+    if (data.length === 0) {
+      throw new Error('Cannot load the database');
+    }
 
+    const students = {};
     let totalStudents = 0;
-    let csStudents = 0;
-    let sweStudents = 0;
-    const csList = [];
-    const sweList = [];
 
-    rows.forEach((row) => {
-      const [firstname, , , field] = row.split(',');
+    for (const line of data) {
+      const [firstName, lastName, field] = line.split(',');
 
-      if (field === 'CS') {
-        csStudents += 1;
-        csList.push(firstname);
-      } else if (field === 'SWE') {
-        sweStudents += 1;
-        sweList.push(firstname);
+      if (firstName && lastName && field) {
+        totalStudents += 1;
+
+        if (!students[field]) {
+          students[field] = [];
+        }
+
+        students[field].push(firstName);
       }
-
-      totalStudents += 1;
-    });
+    }
 
     console.log(`Number of students: ${totalStudents}`);
-    console.log(`Number of students in CS: ${csStudents}. List: ${csList.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents}. List: ${sweList.join(', ')}`);
+
+    for (const field in students) {
+      const count = students[field].length;
+      const list = students[field].join(', ');
+      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+    }
   } catch (error) {
-    throw new Error('Cannot load the database');
+    console.error(error.message);
   }
 }
 
